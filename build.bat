bad98121a50c92a63be684ea28bebd83ab6812d1
Camilla Berglund <elmindreda@elmindreda.org>
2015-04-07 15:37:18 +0200

    Directory name fixes.

--- build.bat
+++ build.bat
@@ -6,78 +6,78 @@ mkdir glfw-bin.WIN64\lib-vc2012
 mkdir glfw-bin.WIN32\lib-vc2013
 mkdir glfw-bin.WIN64\lib-vc2013
 
-mkdir build-vc2010-x86
-cd    build-vc2010-x86
+mkdir build\vc2010-x86
+cd    build\vc2010-x86
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\..\glfw
 nmake
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\..\glfw
 nmake
 
-cd ..
+cd ..\..
 
-copy build-vc2010-x86\src\glfw3.lib    glfw-bin.WIN32\lib-vc2010
-copy build-vc2010-x86\src\glfw3dll.lib glfw-bin.WIN32\lib-vc2010
-copy build-vc2010-x86\src\glfw3.dll    glfw-bin.WIN32\lib-vc2010
+copy build\vc2010-x86\src\glfw3.lib    glfw-bin.WIN32\lib-vc2010
+copy build\vc2010-x86\src\glfw3dll.lib glfw-bin.WIN32\lib-vc2010
+copy build\vc2010-x86\src\glfw3.dll    glfw-bin.WIN32\lib-vc2010
 
-mkdir build-vc2012-x86
-cd    build-vc2012-x86
+mkdir build\vc2012-x86
+cd    build\vc2012-x86
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\..\glfw
 nmake
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\..\glfw
 nmake
 
-cd ..
+cd ..\..
 
-copy build-vc2012-x86\src\glfw3.lib    glfw-bin.WIN32\lib-vc2012
-copy build-vc2012-x86\src\glfw3dll.lib glfw-bin.WIN32\lib-vc2012
-copy build-vc2012-x86\src\glfw3.dll    glfw-bin.WIN32\lib-vc2012
+copy build\vc2012-x86\src\glfw3.lib    glfw-bin.WIN32\lib-vc2012
+copy build\vc2012-x86\src\glfw3dll.lib glfw-bin.WIN32\lib-vc2012
+copy build\vc2012-x86\src\glfw3.dll    glfw-bin.WIN32\lib-vc2012
 
-mkdir build-vc2012-x64
-cd    build-vc2012-x64
+mkdir build\vc2012-x64
+cd    build\vc2012-x64
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\..\glfw
 nmake
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\..\glfw
 nmake
 
-cd ..
+cd ..\..
 
-copy build-vc2012-x64\src\glfw3.lib    glfw-bin.WIN64\lib-vc2012
-copy build-vc2012-x64\src\glfw3dll.lib glfw-bin.WIN64\lib-vc2012
-copy build-vc2012-x64\src\glfw3.dll    glfw-bin.WIN64\lib-vc2012
+copy build\vc2012-x64\src\glfw3.lib    glfw-bin.WIN64\lib-vc2012
+copy build\vc2012-x64\src\glfw3dll.lib glfw-bin.WIN64\lib-vc2012
+copy build\vc2012-x64\src\glfw3.dll    glfw-bin.WIN64\lib-vc2012
 
-mkdir build-vc2013-x86
-cd    build-vc2013-x86
+mkdir build\vc2013-x86
+cd    build\vc2013-x86
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\..\glfw
 nmake
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\..\glfw
 nmake
 
-cd ..
+cd ..\..
 
-copy build-vc2013-x86\src\glfw3.lib    glfw-bin.WIN32\lib-vc2013
-copy build-vc2013-x86\src\glfw3dll.lib glfw-bin.WIN32\lib-vc2013
-copy build-vc2013-x86\src\glfw3.dll    glfw-bin.WIN32\lib-vc2013
+copy build\vc2013-x86\src\glfw3.lib    glfw-bin.WIN32\lib-vc2013
+copy build\vc2013-x86\src\glfw3dll.lib glfw-bin.WIN32\lib-vc2013
+copy build\vc2013-x86\src\glfw3.dll    glfw-bin.WIN32\lib-vc2013
 
-mkdir build-vc2013-x64
-cd    build-vc2013-x64
+mkdir build\vc2013-x64
+cd    build\vc2013-x64
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ..\..\..\glfw
 nmake
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\glfw
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ..\..\..\glfw
 nmake
 
-cd ..
+cd ..\..
 
-copy build-vc2013-x64\src\glfw3.lib    glfw-bin.WIN64\lib-vc2013
-copy build-vc2013-x64\src\glfw3dll.lib glfw-bin.WIN64\lib-vc2013
-copy build-vc2013-x64\src\glfw3.dll    glfw-bin.WIN64\lib-vc2013
+copy build\vc2013-x64\src\glfw3.lib    glfw-bin.WIN64\lib-vc2013
+copy build\vc2013-x64\src\glfw3dll.lib glfw-bin.WIN64\lib-vc2013
+copy build\vc2013-x64\src\glfw3.dll    glfw-bin.WIN64\lib-vc2013
 
--- build.sh
+++ build.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 
-mkdir -p lib-mingw/x86
-mkdir -p lib-mingw/x64
+mkdir -p glfw-bin.WIN32/lib-mingw
+mkdir -p glfw-bin.WIN64/lib-mingw
 
-mkdir -p build-mingw-x86
-pushd    build-mingw-x86
+mkdir -p build/mingw-x86
+pushd    build/mingw-x86
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../../glfw/CMake/i686-pc-mingw32.cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ../../glfw
+cmake -DCMAKE_TOOLCHAIN_FILE=../../../glfw/CMake/i686-pc-mingw32.cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ../../../glfw
 make
-cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ../../glfw
-make
-
-popd
-
-cp build-mingw-x86/src/libglfw3.a lib-mingw/x86
-cp build-mingw-x86/src/glfw3dll.a lib-mingw/x86
-cp build-mingw-x86/src/glfw3.dll  lib-mingw/x86
-
-mkdir -p build-mingw-x64
-pushd    build-mingw-x64
-
-cmake -DCMAKE_TOOLCHAIN_FILE=../../glfw/CMake/x86_64-w64-mingw32.cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ../../glfw
-make
-cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ../../glfw
+cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ../../../glfw
 make
 
 popd
 
-cp build-mingw-x64/src/libglfw3.a lib-mingw/x64
-cp build-mingw-x64/src/glfw3dll.a lib-mingw/x64
-cp build-mingw-x64/src/glfw3.dll  lib-mingw/x64
+cp build/mingw-x86/src/libglfw3.a glfw-bin.WIN32/lib-mingw
+cp build/mingw-x86/src/glfw3dll.a glfw-bin.WIN32/lib-mingw
+cp build/mingw-x86/src/glfw3.dll  glfw-bin.WIN32/lib-mingw
+
+mkdir -p build/mingw-x64
+pushd    build/mingw-x64
+
+cmake -DCMAKE_TOOLCHAIN_FILE=../../../glfw/CMake/x86_64-w64-mingw32.cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=NO ../../../glfw
+make
+cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DBUILD_SHARED_LIBS=YES ../../../glfw
+make
+
+popd
+
+cp build/mingw-x64/src/libglfw3.a glfw-bin.WIN64/lib-mingw
+cp build/mingw-x64/src/glfw3dll.a glfw-bin.WIN64/lib-mingw
+cp build/mingw-x64/src/glfw3.dll  glfw-bin.WIN64/lib-mingw
 
 cmd /c build.bat
 
