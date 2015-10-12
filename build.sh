7a9c2b80388a130f47cef80fd0c2e42acf2bf782
Camilla Berglund <elmindreda@elmindreda.org>
2015-10-12 19:45:14 +0200

    Add separate MinGW and MinGW-w64 builds

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ OPTIONS='-DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=
 GLFWDIR='../../../glfw'
 
 mkdir -p glfw-bin.WIN32/lib-mingw
-mkdir -p glfw-bin.WIN64/lib-mingw
+mkdir -p glfw-bin.WIN32/lib-mingw-w64
+mkdir -p glfw-bin.WIN64/lib-mingw-w64
 
 mkdir -p build/mingw-x86
 pushd    build/mingw-x86
@@ -20,8 +21,23 @@ cp build/mingw-x86/src/libglfw3.a glfw-bin.WIN32/lib-mingw
 cp build/mingw-x86/src/glfw3dll.a glfw-bin.WIN32/lib-mingw
 cp build/mingw-x86/src/glfw3.dll  glfw-bin.WIN32/lib-mingw
 
-mkdir -p build/mingw-x64
-pushd    build/mingw-x64
+mkdir -p build/mingw-w64-x86
+pushd    build/mingw-w64-x86
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$GLFWDIR/CMake/i686-w64-mingw32.cmake $OPTIONS -DBUILD_SHARED_LIBS=NO $GLFWDIR
+make
+cmake $OPTIONS -DBUILD_SHARED_LIBS=YES $GLFWDIR
+make
+
+popd
+
+cp build/mingw-w64-x86/src/libglfw3.a glfw-bin.WIN32/lib-mingw-w64
+cp build/mingw-w64-x86/src/glfw3dll.a glfw-bin.WIN32/lib-mingw-w64
+cp build/mingw-w64-x86/src/glfw3.dll  glfw-bin.WIN32/lib-mingw-w64
+
+
+mkdir -p build/mingw-w64-x64
+pushd    build/mingw-w64-x64
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$GLFWDIR/CMake/x86_64-w64-mingw32.cmake $OPTIONS -DBUILD_SHARED_LIBS=NO $GLFWDIR
 make
@@ -30,9 +46,9 @@ make
 
 popd
 
-cp build/mingw-x64/src/libglfw3.a glfw-bin.WIN64/lib-mingw
-cp build/mingw-x64/src/glfw3dll.a glfw-bin.WIN64/lib-mingw
-cp build/mingw-x64/src/glfw3.dll  glfw-bin.WIN64/lib-mingw
+cp build/mingw-w64-x64/src/libglfw3.a glfw-bin.WIN64/lib-mingw-w64
+cp build/mingw-w64-x64/src/glfw3dll.a glfw-bin.WIN64/lib-mingw-w64
+cp build/mingw-w64-x64/src/glfw3.dll  glfw-bin.WIN64/lib-mingw-w64
 
 cmd /c build.bat
 
