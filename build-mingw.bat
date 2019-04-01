d304259af503b309616f438149952614bf82d2eb
Camilla Löwy <elmindreda@elmindreda.org>
2019-04-01 02:12:37 +0200

    Add plain MinGW build script

---
+++ build-mingw.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set COMMON=-DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DCMAKE_C_FLAGS=-s
+set SHARED=%COMMON% -DBUILD_SHARED_LIBS=YES
+set STATIC=%COMMON% -DBUILD_SHARED_LIBS=NO
+set GLFWDIR="..\..\..\glfw"
+
+rem MinGW
+set GENERATOR="MinGW Makefiles"
+set BUILDDIR="build\mingw-x86"
+set TARGETDIR="glfw-bin.WIN32\lib-mingw"
+
+cmake -E make_directory %BUILDDIR%
+cmake -E make_directory %TARGETDIR%
+cmake -E chdir %BUILDDIR% cmake -G %GENERATOR% %STATIC% %GLFWDIR%
+cmake --build %BUILDDIR% --config Release
+cmake -E chdir %BUILDDIR% cmake %SHARED% %GLFWDIR%
+cmake --build %BUILDDIR% --config Release
+cmake -E copy %BUILDDIR%\src\libglfw3.a    %TARGETDIR%
+cmake -E copy %BUILDDIR%\src\libglfw3dll.a %TARGETDIR%
+cmake -E copy %BUILDDIR%\src\glfw3.dll     %TARGETDIR%
+
