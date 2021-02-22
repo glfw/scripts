fae731b4aca041f4103b1f860b007b8631d0551d
Camilla Löwy <elmindreda@elmindreda.org>
2021-02-22 23:08:54 +0100

    Simplify quoting in BAT files

--- build-mingw.bat
+++ build-mingw.bat
@@ -3,26 +3,26 @@
 set COMMON=-DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO -DCMAKE_C_FLAGS=-s
 set SHARED=%COMMON% -DBUILD_SHARED_LIBS=YES
 set STATIC=%COMMON% -DBUILD_SHARED_LIBS=NO
-set GLFWDIR="%1" 
-set GLFWVER="%2"
+set GLFWDIR=%1 
+set GLFWVER=%2
 
 if "%GLFWVER%" EQU "" exit /b 1
 if not exist "%GLFWDIR%" exit /b 1
 
-set GENERATOR="MinGW Makefiles"
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-mingw"
+set GENERATOR=MinGW Makefiles
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-mingw
 
-set BUILDDIR="build\mingw-static"
+set BUILDDIR=build\mingw-static
 cmake -E make_directory %BUILDDIR%
 cmake -E make_directory %TARGETDIR%
-cmake -S "%GLFWDIR%" -B %BUILDDIR% -G %GENERATOR% %STATIC%
+cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" %STATIC%
 cmake --build %BUILDDIR%
 cmake -E copy %BUILDDIR%\src\libglfw3.a %TARGETDIR%
 
-set BUILDDIR="build\mingw-dll"
+set BUILDDIR=build\mingw-dll
 cmake -E make_directory %BUILDDIR%
 cmake -E make_directory %TARGETDIR%
-cmake -S "%GLFWDIR%" -B %BUILDDIR% -G %GENERATOR% %SHARED%
+cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" %SHARED%
 cmake --build %BUILDDIR%
 cmake -E copy %BUILDDIR%\src\libglfw3dll.a %TARGETDIR%
 cmake -E copy %BUILDDIR%\src\glfw3.dll     %TARGETDIR%
--- build-vs.bat
+++ build-vs.bat
@@ -3,97 +3,97 @@
 set COMMON=-DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO
 set SHARED=%COMMON% -DBUILD_SHARED_LIBS=YES
 set STATIC=%COMMON% -DBUILD_SHARED_LIBS=NO
-set GLFWDIR="%1" 
-set GLFWVER="%2"
+set GLFWDIR=%1 
+set GLFWVER=%2
 
 if "%GLFWVER%" EQU "" exit /b 1
 if not exist "%GLFWDIR%" exit /b 1
 
 rem Visual C++ 2010 32-bit
-set GENERATOR="Visual Studio 10 2010"
-set ARCH="Win32"
+set GENERATOR=Visual Studio 10 2010
+set ARCH=Win32
 set TOOLSET=v100
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-vc2010"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2010
 call :build_static
 call :build_dll
 
 rem Visual C++ 2012 32-bit
-set GENERATOR="Visual Studio 11 2012"
-set ARCH="Win32"
+set GENERATOR=Visual Studio 11 2012
+set ARCH=Win32
 set TOOLSET=v110_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-vc2012"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2012
 call :build_static
 call :build_dll
 
 rem Visual C++ 2012 64-bit
-set GENERATOR="Visual Studio 11 2012"
-set ARCH="x64"
+set GENERATOR=Visual Studio 11 2012
+set ARCH=x64
 set TOOLSET=v110_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN64\lib-vc2012"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2012
 call :build_static
 call :build_dll
 
 rem Visual C++ 2013 32-bit
-set GENERATOR="Visual Studio 12 2013"
-set ARCH="Win32"
+set GENERATOR=Visual Studio 12 2013
+set ARCH=Win32
 set TOOLSET=v120_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-vc2013"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2013
 call :build_static
 call :build_dll
 
 rem Visual C++ 2013 64-bit
-set GENERATOR="Visual Studio 12 2013"
-set ARCH="x64"
+set GENERATOR=Visual Studio 12 2013
+set ARCH=x64
 set TOOLSET=v120_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN64\lib-vc2013"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2013
 call :build_static
 call :build_dll
 
 rem Visual C++ 2015 32-bit
-set GENERATOR="Visual Studio 14 2015"
-set ARCH="Win32"
+set GENERATOR=Visual Studio 14 2015
+set ARCH=Win32
 set TOOLSET=v140_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-vc2015"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2015
 call :build_static
 call :build_dll
 
 rem Visual C++ 2015 64-bit
-set GENERATOR="Visual Studio 14 2015"
-set ARCH="x64"
+set GENERATOR=Visual Studio 14 2015
+set ARCH=x64
 set TOOLSET=v140_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN64\lib-vc2015"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2015
 call :build_static
 call :build_dll
 
 rem Visual C++ 2017 32-bit
-set GENERATOR="Visual Studio 15 2017"
-set ARCH="Win32"
+set GENERATOR=Visual Studio 15 2017
+set ARCH=Win32
 set TOOLSET=v141_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-vc2017"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2017
 call :build_static
 call :build_dll
 
 rem Visual C++ 2017 64-bit
-set GENERATOR="Visual Studio 15 2017"
-set ARCH="x64"
+set GENERATOR=Visual Studio 15 2017
+set ARCH=x64
 set TOOLSET=v141_xp
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN64\lib-vc2017"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2017
 call :build_static
 call :build_dll
 
 rem Visual C++ 2019 32-bit
-set GENERATOR="Visual Studio 16 2019"
-set ARCH="Win32"
+set GENERATOR=Visual Studio 16 2019
+set ARCH=Win32
 set TOOLSET=v142
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN32\lib-vc2019"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2019
 call :build_static
 call :build_dll
 
 rem Visual C++ 2019 64-bit
-set GENERATOR="Visual Studio 16 2019"
-set ARCH="x64"
+set GENERATOR=Visual Studio 16 2019
+set ARCH=x64
 set TOOLSET=v142
-set TARGETDIR="glfw-%GLFWVER%.bin.WIN64\lib-vc2019"
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2019
 call :build_static
 call :build_dll
 
@@ -103,7 +103,7 @@ exit /b 0
 set BUILDDIR=build\vs-%TOOLSET%-%ARCH%-static
 cmake -E make_directory %BUILDDIR%
 cmake -E make_directory %TARGETDIR%
-cmake -S "%GLFWDIR%" -B %BUILDDIR% -G %GENERATOR% -A %ARCH% -T %TOOLSET% %STATIC%
+cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" -A %ARCH% -T %TOOLSET% %STATIC%
 cmake --build %BUILDDIR% --config Release
 cmake -E copy %BUILDDIR%\src\Release\glfw3.lib %TARGETDIR%
 exit /b 0
@@ -112,7 +112,7 @@ exit /b 0
 set BUILDDIR=build\vs-%TOOLSET%-%ARCH%-dll
 cmake -E make_directory %BUILDDIR%
 cmake -E make_directory %TARGETDIR%
-cmake -S "%GLFWDIR%" -B %BUILDDIR% -G %GENERATOR% -A %ARCH% -T %TOOLSET% %SHARED%
+cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" -A %ARCH% -T %TOOLSET% %SHARED%
 cmake --build %BUILDDIR% --config Release
 cmake -E copy %BUILDDIR%\src\Release\glfw3dll.lib %TARGETDIR%
 cmake -E copy %BUILDDIR%\src\Release\glfw3.dll    %TARGETDIR%
