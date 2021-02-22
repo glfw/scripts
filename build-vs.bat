bc93824aca5aae3a9ba985641c836650926f963f
Camilla Löwy <elmindreda@elmindreda.org>
2021-02-22 23:11:23 +0100

    Add Windows binaries for VS static CRT
    Related to glfw/glfw#528.

--- build-vs.bat
+++ build-vs.bat
@@ -3,6 +3,7 @@
 set COMMON=-DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO
 set SHARED=%COMMON% -DBUILD_SHARED_LIBS=YES
 set STATIC=%COMMON% -DBUILD_SHARED_LIBS=NO
+set MT=-DUSE_MSVC_RUNTIME_LIBRARY_DLL=OFF
 set GLFWDIR=%1 
 set GLFWVER=%2
 
@@ -15,6 +16,7 @@ set ARCH=Win32
 set TOOLSET=v100
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2010
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2012 32-bit
@@ -23,6 +25,7 @@ set ARCH=Win32
 set TOOLSET=v110_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2012
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2012 64-bit
@@ -31,6 +34,7 @@ set ARCH=x64
 set TOOLSET=v110_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2012
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2013 32-bit
@@ -39,6 +43,7 @@ set ARCH=Win32
 set TOOLSET=v120_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2013
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2013 64-bit
@@ -47,6 +52,7 @@ set ARCH=x64
 set TOOLSET=v120_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2013
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2015 32-bit
@@ -55,6 +61,7 @@ set ARCH=Win32
 set TOOLSET=v140_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2015
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2015 64-bit
@@ -63,6 +70,7 @@ set ARCH=x64
 set TOOLSET=v140_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2015
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2017 32-bit
@@ -71,6 +79,7 @@ set ARCH=Win32
 set TOOLSET=v141_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2017
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2017 64-bit
@@ -79,6 +88,7 @@ set ARCH=x64
 set TOOLSET=v141_xp
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2017
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2019 32-bit
@@ -87,6 +97,7 @@ set ARCH=Win32
 set TOOLSET=v142
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-vc2019
 call :build_static
+call :build_static_mt
 call :build_dll
 
 rem Visual C++ 2019 64-bit
@@ -95,7 +106,22 @@ set ARCH=x64
 set TOOLSET=v142
 set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-vc2019
 call :build_static
+call :build_static_mt
 call :build_dll
+
+rem Visual C++ 2019 32-bit DLL with static UCRT
+set GENERATOR=Visual Studio 16 2019
+set ARCH=Win32
+set TOOLSET=v142
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN32\lib-static-ucrt
+call :build_dll_mt
+
+rem Visual C++ 2019 64-bit with static UCRT
+set GENERATOR=Visual Studio 16 2019
+set ARCH=x64
+set TOOLSET=v142
+set TARGETDIR=glfw-%GLFWVER%.bin.WIN64\lib-static-ucrt
+call :build_dll_mt
 
 exit /b 0
 
@@ -108,11 +134,30 @@ cmake --build %BUILDDIR% --config Release
 cmake -E copy %BUILDDIR%\src\Release\glfw3.lib %TARGETDIR%
 exit /b 0
 
+:build_static_mt
+set BUILDDIR=build\vs-%TOOLSET%-%ARCH%-static-mt
+cmake -E make_directory %BUILDDIR%
+cmake -E make_directory %TARGETDIR%
+cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" -A %ARCH% -T %TOOLSET% %STATIC% %MT%
+cmake --build %BUILDDIR% --config Release
+cmake -E copy %BUILDDIR%\src\Release\glfw3.lib %TARGETDIR%\glfw3_mt.lib
+exit /b 0
+
 :build_dll
 set BUILDDIR=build\vs-%TOOLSET%-%ARCH%-dll
 cmake -E make_directory %BUILDDIR%
 cmake -E make_directory %TARGETDIR%
 cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" -A %ARCH% -T %TOOLSET% %SHARED%
+cmake --build %BUILDDIR% --config Release
+cmake -E copy %BUILDDIR%\src\Release\glfw3dll.lib %TARGETDIR%
+cmake -E copy %BUILDDIR%\src\Release\glfw3.dll    %TARGETDIR%
+exit /b 0
+
+:build_dll_mt
+set BUILDDIR=build\vs-%TOOLSET%-%ARCH%-dll-mt
+cmake -E make_directory %BUILDDIR%
+cmake -E make_directory %TARGETDIR%
+cmake -S "%GLFWDIR%" -B %BUILDDIR% -G "%GENERATOR%" -A %ARCH% -T %TOOLSET% %SHARED% %MT%
 cmake --build %BUILDDIR% --config Release
 cmake -E copy %BUILDDIR%\src\Release\glfw3dll.lib %TARGETDIR%
 cmake -E copy %BUILDDIR%\src\Release\glfw3.dll    %TARGETDIR%
