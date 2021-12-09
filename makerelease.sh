0957b0d558c07e69ebb476c7a645d8817b02de8a
Camilla Löwy <elmindreda@elmindreda.org>
2021-12-09 23:03:45 +0100

    Update compiler lists

--- makerelease.sh
+++ makerelease.sh
@@ -125,9 +125,9 @@ import libraries and DLLs for GLFW ${tag}.
 
 Binaries for the following compilers are included
 
- - Visual C++ 2022 (built with 17.0.0 preview 7.0)
- - Visual C++ 2019 (built with 16.11.5)
- - Visual C++ 2017 (built with 15.9.40)
+ - Visual C++ 2022 (built with 17.1.0 preview 1.1)
+ - Visual C++ 2019 (built with 16.11.7)
+ - Visual C++ 2017 (built with 15.9.41)
  - Visual C++ 2015 (built with 14.0.25431.01)
  - Visual C++ 2013 (built with 12.0.40629.00)
  - Visual C++ 2012 (built with 11.0.61219.00)
@@ -199,9 +199,9 @@ import libraries and DLLs for GLFW ${tag}.
 
 Binaries for the following compilers are included
 
- - Visual C++ 2022 (built with 17.0.0 preview 7.0)
- - Visual C++ 2019 (built with 16.11.5)
- - Visual C++ 2017 (built with 15.9.40)
+ - Visual C++ 2022 (built with 17.1.0 preview 1.1)
+ - Visual C++ 2019 (built with 16.11.7)
+ - Visual C++ 2017 (built with 15.9.41)
  - Visual C++ 2015 (built with 14.0.25431.01)
  - Visual C++ 2013 (built with 12.0.40629.00)
  - Visual C++ 2012 (built with 11.0.61219.00)
